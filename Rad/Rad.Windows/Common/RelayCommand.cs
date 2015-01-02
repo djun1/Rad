1563d8389bee80bc06ebc969a771071f5a6cbbe2
clueso <clueso@gmail.com>
2015-01-02 13:53:36 -0800

    Removed References to Sat namespace
    Removed Sat namespace references from Windows common code.

--- Rad/Rad.Windows/Common/RelayCommand.cs
+++ Rad/Rad.Windows/Common/RelayCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Sat.Common
+namespace Rad.Common
 {
     /// <summary>
     /// A command whose sole purpose is to relay its functionality 
--- Rad/Rad.Windows/MainPage.xaml.cs
+++ Rad/Rad.Windows/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Sat.Common;
+﻿using Rad.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
